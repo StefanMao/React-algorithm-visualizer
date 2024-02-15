90f3415bb17337a227325078c049ce457c8ea1b5
stefanmao <stefanmao1112@gmail.com>
2024-02-15 23:28:02 +0800

    chore: Add @types/node and  vite-tsconfig-paths plugins to refactor import path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  esbuild: {
+    loader: 'tsx',
+  },
+  root: './',
+  build: {
+    outDir: './build',
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.ts': 'tsx',
+      },
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+});
